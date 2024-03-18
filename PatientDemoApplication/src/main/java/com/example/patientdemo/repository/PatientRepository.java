ed9c4c411483484f07ffeb81c587dcb80600ca3a
Bimal Behera <76647449+bbehera277@users.noreply.github.com>
2024-03-18 06:15:39 -0700

    Delete PatientDemoApplication directory

--- PatientDemoApplication/src/main/java/com/example/patientdemo/repository/PatientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.patientdemo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.patientdemo.data.Patient;
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-
-}
